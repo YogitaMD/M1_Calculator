494080347a605f6c18150a62ed5811cf27bfd067
YogitaMD <97722998+YogitaMD@users.noreply.github.com>
2022-04-02 00:57:04 -0700

    updated

---
+++ 3_Implementation/inc/calculator.h
@@ -0,0 +1,18 @@
+/** 
+* @file calculator.h
+*
+*/
+
+#ifndef __CALCULATOR_H__
+#define __CALCULATOR_H__
+
+
+#include "addition.h"
+#include "substraction.h"
+#include "multiplication.h"
+#include "division.h"
+#include "squareroot.h"
+#include "power.h"
+#include "modulus.h"
+
+#endif  //__CALCULATOR_H__
