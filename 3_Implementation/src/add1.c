5b2cdbdbe9db69e948b904362dbf8a2647717481
YogitaMD <97722998+YogitaMD@users.noreply.github.com>
2022-04-02 00:57:41 -0700

    Add files via upload

---
+++ 3_Implementation/src/add1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+float answ;
+float add(float operand1, float operand2) //addition of two numbers
+{
+    answ = operand1 + operand2;
+    return answ;
+}
---
+++ 3_Implementation/src/multi1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+float ansr;
+float multi(float operand1, float operand2) //multiplication of two numbers
+{
+    ansr = operand1 * operand2;
+    return ansr;
+}
